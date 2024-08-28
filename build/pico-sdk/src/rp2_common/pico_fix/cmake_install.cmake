96170ee54b3f9cf909522b43ac7865221870555d
akcase18 <aidan.case@casefamily.us>
2024-08-28 12:47:52 -0600

    Maybe it'll work now!

--- build/pico-sdk/src/rp2_common/pico_fix/cmake_install.cmake
+++ build/pico-sdk/src/rp2_common/pico_fix/cmake_install.cmake
@@ -39,6 +39,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/aidancase/blink/build/pico-sdk/src/rp2_common/pico_fix/rp2040_usb_device_enumeration/cmake_install.cmake")
+  include("/Users/aidancase/ece5785_lab0/build/pico-sdk/src/rp2_common/pico_fix/rp2040_usb_device_enumeration/cmake_install.cmake")
 endif()
 
